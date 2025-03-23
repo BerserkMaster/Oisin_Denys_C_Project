bff65077fbf56cd461020147c20ff47f00a0449f
Denys Kapustianskyi <denyskapustianskyi@icloud.com>
2025-03-23 21:38:09 +0000

    added new functions

--- project.c
+++ project.c
@@ -3,10 +3,25 @@
 #include <time.h>      //for random generation
 
 int randomNumGeneration();
+int interWithUserO(int* );  // O turn
+int interWithUserX(int* );  // X turn
 
 int main() {
-    printf("A random digit is: %d ", randomNumGeneration());
+    int counter = 0;
+    int turn = 0;
+    int *ptrTurn = &turn;
 
+    randomNumGeneration();
+
+    while (counter < 9){
+      if (counter % 2 == 0) {
+        interWithUserO(ptrTurn);
+      } else if (counter % 2 != 0) {
+        interWithUserX(ptrTurn);
+      } 
+
+      counter++;
+    } 
 
 
     return 0;
@@ -17,4 +32,15 @@ int randomNumGeneration(){
     srand(time(NULL));  //seed for a generation function
     randNum = rand() % 9 + 1;  //generate a digit within specific limit 
     return randNum;
+}
+
+int interWithUserO(int* o){
+    printf("Please enter a digit of cell to draw O: ");
+    scanf("%d", o);
+}
+
+int interWithUserX(int* x){
+    int choice = 0;
+    printf("Please enter a digit of cell to draw X: ");
+    scanf("%d", x);
 }
