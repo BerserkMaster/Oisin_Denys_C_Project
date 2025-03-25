40e5c043cdd3ec0f1811c110e56343573c0d436f
Denys Kapustianskyi <denyskapustianskyi@icloud.com>
2025-03-25 22:12:51 +0000

    added two new functions, they work perfectly

--- main.c
+++ main.c
@@ -9,6 +9,9 @@ void computerMove();
 void clearBoard();
 void checkSpaces();
 void checkWinner();
+void check_data();  //check the ranking list
+void save_data(char name[], int score);  //save data to a ranking list
+
 
 char board[SIZE][SIZE];
 
@@ -48,3 +51,55 @@ void printBoard() {
 	printf("\n -- | -- | --\n");
 	printf(" %c | %c | %c ", board[2][0], board[2][1], board[2][2]);
 }
+
+void check_data() {
+  FILE* file = fopen("game_data.txt", "r");
+  char ch;
+  while ((ch = fgetc(file)) != EOF) {  // read until the end of the file
+    putchar(ch);  // similar to printf function
+  }
+}
+
+void save_data(char name[], int score) {
+    FILE* file = fopen("game_data.txt", "r");
+    if (file == NULL) {
+        perror("File opening failed");
+        return;
+    }
+
+    FILE* tempFile = fopen("temp_game_data.txt", "w");
+    if (tempFile == NULL) {
+        perror("Temporary file opening failed");
+        fclose(file);
+        return;
+    }
+
+    char line[30];  
+    int found = 0;
+
+    while (fgets(line, sizeof(line), file)) {
+      char exstracted_name[15];
+      int exstracted_score = 0;
+      if (sscanf(line, "%s - %d", exstracted_name, &exstracted_score) == 2) {
+        if (strcmp(exstracted_name, name) == 0) {
+          exstracted_score += score;
+          fprintf(tempFile, "%s - %d\n", exstracted_name, exstracted_score);
+          found = 1;
+        } else {
+          fputs(line, tempFile);
+        }
+      } else {
+        fputs(line, tempFile);
+      }
+    }
+
+    if (!found) {
+        fprintf(tempFile, "%s - %d\n", name, score);
+    }
+
+    fclose(file);
+    fclose(tempFile);
+
+    remove("game_data.txt");
+    rename("temp_game_data.txt", "game_data.txt");
+  }
