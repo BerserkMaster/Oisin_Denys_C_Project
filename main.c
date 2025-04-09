cf4d1525920d597f4afaafca7d2efcc92bd4eae0
OisinATU <G00470518@atu.ie>
2025-04-09 19:46:19 +0100

    Draw scenario handled, added PrintResult function to handle two player results

--- main.c
+++ main.c
@@ -14,13 +14,18 @@ void updateBoard(int, char);
 void check_data();                      // check the ranking list
 void save_data(char name[], int score); // save data to a ranking list
 void printLogo();
-void printVictory();
+void printVictory(char name[]);
 void printDefeat();
+void printDraw();
 void printTopPlayers();
-int randomNumGeneration();
+void printResult(bool singlePlayer, int turn, char name[]);
+void togglePlayer(char *currentPlayer);
+int randomNumGeneration(int range);
 int interWithUserO(const char* user); // O turn
 int interWithUserX(const char* user); // X turn:
-int checkWinner(int);
+int checkWinner(int data);
+
+
 
 struct Name
 {
@@ -37,6 +42,8 @@ int main()
   int user2Input = 0;
   int robotTurn = 0;
   int turn = 0;
+  char currentPlayer = 'X';
+  bool singlePlayer = false;
 
   printLogo();
 
@@ -48,6 +55,7 @@ int main()
   switch (userInput)
   {
   case 1:
+    singlePlayer= true;
     printf("Enter your name: ");
     scanf("%s", userData[1].name);
     resetBoard();
@@ -55,7 +63,7 @@ int main()
     while (winner == 0)
     {
       printf("\n");
-      if (turn % 2 == 0)
+      if (currentPlayer == 'X' )
       {
         while (winner == 0)
         {
@@ -75,6 +83,7 @@ int main()
             winner = checkWinner(turn);
             if (winner == 1)
             {
+              printResult(singlePlayer, turn,userData[1].name);
               save_data(userData[1].name, 1);
             }
             turn++;
@@ -86,12 +95,12 @@ int main()
       {
         while (winner == 0)
         {
-          robotTurn = randomNumGeneration();
+          robotTurn = randomNumGeneration(9);
           int row = (robotTurn - 1) / SIZE;
           int col = (robotTurn - 1) % SIZE;
           if (board[row][col] == 'X' || board[row][col] == 'O')
           {
-            robotTurn = randomNumGeneration();
+            robotTurn = randomNumGeneration(9);
           }
           else
           {
@@ -101,6 +110,7 @@ int main()
             winner = checkWinner(turn);
             if (winner == 1)
             {
+              printResult(singlePlayer, turn, userData[1].name);
               save_data(userData[1].name, -1);
             }
             turn++;
@@ -110,7 +120,14 @@ int main()
       }
     }
     break;
+
+
+
+
+
+
   case 2:
+    singlePlayer = false;
     printf("Player 1 enter your name: ");
     scanf("%s", userData[2].name);
     save_data(userData[2].name, userData[2].score);
@@ -123,7 +140,7 @@ int main()
     while (winner == 0)
     {
       printf("\n");
-      if (turn % 2 == 0)
+      if (currentPlayer=='X')
       {
         while (winner == 0)
         {
@@ -140,12 +157,15 @@ int main()
             updateBoard(userInput, 'X');
             printBoard();
             puts("\n");
+
             winner = checkWinner(turn);
             if (winner == 1)
             {
+              printResult(singlePlayer, turn, userData[2].name);
               save_data(userData[2].name, 1);
               save_data(userData[3].name, -1);
-            }
+              }
+            togglePlayer(&currentPlayer);
             turn++;
             break;
           }
@@ -171,9 +191,11 @@ int main()
             winner = checkWinner(turn);
             if (winner == 1)
             {
+              printResult(singlePlayer, turn, userData[3].name);
               save_data(userData[3].name, 1);
               save_data(userData[2].name, -1);
             }
+            togglePlayer(&currentPlayer);
             turn++;
             break;
           }
@@ -181,6 +203,12 @@ int main()
       }
     }
     break;
+
+
+
+
+
+
 
 
 
@@ -192,11 +220,11 @@ int main()
   return 0;
 }
 
-int randomNumGeneration()
+int randomNumGeneration(int num)
 {
   int randNum = 0;
   srand(time(NULL));        // seed for a generation function
-  randNum = rand() % 9 + 1; // generate a digit within specific limit
+  randNum = rand() % num + 1; // generate a digit within specific limit
   return randNum;
 }
 
@@ -349,16 +377,9 @@ int checkWinner(int data)
   {
     if (board[row][0] == board[row][1] && board[row][1] == board[row][2])
     {
-      if (data % 2 == 0)
-      {
-        printVictory();
-      }
-      else
-      {
-        puts("You lost, game is over");
-      }
       return 1;
     }
+
   }
 
   // check columns
@@ -366,14 +387,6 @@ int checkWinner(int data)
   {
     if (board[0][col] == board[1][col] && board[1][col] == board[2][col])
     {
-      if (data % 2 == 0)
-      {
-        printVictory();
-      }
-      else
-      {
-        printDefeat();
-      }
       return 1;
     }
   }
@@ -381,30 +394,43 @@ int checkWinner(int data)
   // check diagonals
   if (board[0][0] == board[1][1] && board[1][1] == board[2][2])
   {
-    if (data % 2 == 0)
-    {
-      printVictory();
-    }
-    else
-    {
-      printDefeat();
-    }
     return 1;
   }
   if (board[2][0] == board[1][1] && board[1][1] == board[0][2])
   {
-    if (data % 2 == 0)
+    return 1;
+  }
+  // check draw
+  if(data==8){
+    return 1;
+  }
+  return 0;
+}
+
+void togglePlayer(char *player) {
+  *player = (*player == 'X') ? 'O' : 'X';
+}
+
+void printResult(bool singlePlayer, int turn, char name[]){
+  if(singlePlayer){
+    if (turn % 2 == 0)
     {
-      printVictory();
+      printVictory(name);
     }
     else
     {
       printDefeat();
     }
-    return 1;
   }
-  return 0;
+  else{
+    if(turn!=8){
+      printVictory(name);
+    } else{
+      printDraw();
+    }
+  }
 }
+
 
 void printLogo()
 {
@@ -426,8 +452,20 @@ void printLogo()
   printf("\n");
 }
 
-void printVictory()
+void printVictory(char name[])
 {
+  const char* victoryMessages[] = {
+    "%s, you are unbeatable!",
+    "Incredible job, %s!",
+    "You are the champion, %s!",
+    "You are amazing, %s!",
+    "%s just swept the board!"
+};
+int numMessages = sizeof(victoryMessages) / sizeof(victoryMessages[0]);
+
+// Choose a random message
+int msgIndex = randomNumGeneration(numMessages);
+
   const char* lines[] = {
         " __     _____ ____ _____ ___  ______   __",
         " \\ \\   / /_ _/ ___|_   _/ _ \\|  _ \\ \\ / /",
@@ -440,7 +478,14 @@ void printVictory()
         printf("%s\n", lines[i]); // prints out lines in array
         usleep(1000000); // Speed it prints at
     }
+
+    printf("\n");
+    printf("\t");
+    printf(victoryMessages[msgIndex], name);
+    printf("\n");
 }
+
+
 
 void printDefeat()
 {
@@ -449,6 +494,24 @@ void printDefeat()
   printf(" | | | |  _| | |_  |  _|   / _ \\ | |  \n");
   printf(" | |_| | |___|  _| | |___ / ___ \\| |  \n");
   printf(" |____/|_____|_|   |_____/_/   \\_\\_|  \n");
+}
+
+
+
+void printDraw()
+{
+    const char* lines[] = {
+        "  ____                     ",
+        " |  _ \\ _ __ __ ___      __",
+        " | | | | '__/ _` \\ \\ /\\ / /",
+        " | |_| | | | (_| |\\ V  V / ",
+        " |____/|_|  \\__,_| \\_/\\_/  ",
+    };
+
+    for (int i = 0; i < 5; i++) {
+        printf("%s\n", lines[i]); // prints out lines in array
+        usleep(1000000); // Speed it prints at, 1 second between lines
+    }
 }
 
 void printTopPlayers()
